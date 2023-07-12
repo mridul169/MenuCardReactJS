0c8eb100a749cdc92995ec6c41ea48999c56be13
mridul169 <mridul.dehradun@gmail.com>
2023-07-12 21:09:40 +0530

    first commit

---
+++ src/component/basics/menuAPI.js
@@ -0,0 +1,95 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/pancake.png",
+    name: "pancake",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "lorem31 jaasulfgd gfahdsg dsagfyugda dafasdf dasfad ydsagfudshfkudshuchdsuf sdygfdysgf iusdgfuidgfgdshfgsufyfhjdbsdjh djs ",
+  },
+
+  {
+    id: 2,
+    image: "images/salad.png",
+    name: "salad",
+    category: "mid-day-snacks",
+    price: "20₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "images/doughnut.png",
+    name: "doughnut",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "../images/biryani.png",
+    name: "biryani",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "../images/noodles.png",
+    name: "noodles",
+    category: "mid-day-snacks",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "../images/prawns.png",
+    name: "prawns",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "../images/barbeque.png",
+    name: "barbeque",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "../images/dinnerSalad.png",
+    name: "dinnerSalad",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "../images/salmon.png",
+    name: "Salmon-Fish",
+    category: "lunch",
+    price: "1000₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "../images/steak.png",
+    name: "Steak",
+    category: "lunch",
+    price: "1000₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
